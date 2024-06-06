3b13368bc1c690028082e8aee6654132e66503a6
HridaySarder <hridoyaug12@gmail.com>
2024-06-06 19:33:43 +0600

    added capacity

---
+++ capacity.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "Hello_World";
+    // s.clear();
+    // cout << s.size() << endl;
+    // cout << s.max_size() << endl;
+    // cout << s.capacity() << endl;
+    // return 0;
+    // if(s.empty()==true)
+    // {
+    //     cout<<"Empty"<<endl;
+    // }
+    // else{
+    //     cout<<"Not empty"<<endl;
+    // }
+    // s.resize(3);
+    s.resize(15, 'X');
+    cout << s << endl;
+}
