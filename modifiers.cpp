ab79bd0758fb14c42851a5fb0d10f078cef7640a
HridaySarder <hridoyaug12@gmail.com>
2024-06-06 22:03:29 +0600

    added modifiers2

--- modifiers.cpp
+++ modifiers.cpp
@@ -4,13 +4,18 @@ int main()
 {
     string a = "Hriday";
     string b = "Sarder";
-    a+=b;
+    // a+=b;
     // a.append(b);
     // cout<<a<<endl;
     // a="HridaySarder";
     // a.push_back('S');
-    a.pop_back();
-    a.pop_back();
+    // a.pop_back();
+    // a.pop_back();
+    // a="Sarder";
+    // a.assign(b);
+    // a.erase(4,1);
+    // a.insert(6,"Sarder");
+    a.replace(6, 0, "Talha");
     cout << a << endl;
     return 0;
 }
